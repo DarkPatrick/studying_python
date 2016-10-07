a1894e0735f5a4396709442fdc2115529e271f08
DarkPatrick <egorsiomin@yandex.ru>
2016-10-07 09:51:36 +0300

    new tabs
    tab indentation has been changed to 4 spaces according with pep8

--- Dejkstra_algorithm/.spyproject/main.py
+++ Dejkstra_algorithm/.spyproject/main.py
@@ -22,16 +22,18 @@ graph_file_name = input("введите имя файла с графом: ")
 graph_file = open(graph_file_name, 'r')
 i = 0
 for line in graph_file:
-  file_str = line
-  if (0 == i):
-# количество веhiby в графе
-    graph_size = int(file_str)
+    file_str = line
+    if (0 == i):
+# количество вершин в графе
+        graph_size = int(file_str)
 # массив вешин из списков расстояний до тех вершин, с которыми имеется связь
-    graph = np.array({} for i in range(graph_size))
-  else:
-    line_str_arr = file_str.split(' ');
-    line_int_arr = [int(num) for num in line_str_arr]
-  i += 1
+        graph = np.array({} for i in range(graph_size))
+    else:
+        line_str_arr = file_str.split(' ');
+        line_int_arr = [int(num) for num in line_str_arr]
+        for j in line_int_arr:
+            #if ()
+    i += 1
 
 
 # номер стартовой вершины
